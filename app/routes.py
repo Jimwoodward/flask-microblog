494091bbd64e51c8dbaeb81e504ba7076a7e225b
Jim Woodward <jim.woodward@snagajob.com>
2019-07-31 16:25:35 -0400

    update url_for function calls because of a syntax error

--- app/routes.py
+++ app/routes.py
@@ -26,5 +26,5 @@ def login():
   if form.validate_on_submit():
     flash('Login requested for for user {}, remember_me={}'.format(
       form.username.data, form.remember_me.data))
-    return redirect(url_for('/index'))
+    return redirect(url_for('index'))
   return render_template('login.html', title='Login', form=form)
