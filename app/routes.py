1fc5983b4a727029c26d426bf284c718a4252c98
Jim Woodward <jim.woodward@snagajob.com>
2019-07-31 15:27:58 -0400

    add login route

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
 # From the app package we are importing the app flask object
 from app import app
+from app.forms import LoginForm
 
 # The syntax here is that the '@app.route' decoration creates an association between the url that's passed in as the parameter with the following function
 @app.route('/')
@@ -18,3 +19,8 @@ def index():
       }
   ]
   return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+  form = LoginForm()
+  render_template('login.html', title='Login', form=form)
